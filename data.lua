6eda79a2a705885e895e44b308dbff5b5947b8f4
GeekinaCave <jarelton85@gmail.com>
2017-11-28 21:45:43 -0500

    INITIAL

---
+++ data.lua
@@ -0,0 +1,20 @@
+data.raw["utility-sprites"]["default"].pollution_visualization.filename = "__My_own_pollution__/graphics/white-pollution-visualization.png"
+
+-- Source data
+
+-- Factorio\data\core\prototypes\utility-sprites.lua
+
+-- data:extend({
+--   {
+--     type = "utility-sprites",
+--     name = "default",
+
+--     pollution_visualization =
+--     {
+--       filename = "__My_own_pollution__/graphics/white-pollution-visualization.png",
+--       priority = "extra-high-no-scale",
+--       width = 32,
+--       height = 32
+--     },
+--   }
+-- })
